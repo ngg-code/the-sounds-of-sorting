64ad1aa53e800fbe30b8dfc81844b8f2aee84005
ngg-code <157342451+ngg-code@users.noreply.github.com>
2025-03-29 01:16:27 +0000

    Implemented CopyEvent class

--- src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/CopyEvent.java
+++ src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/CopyEvent.java
@@ -1,8 +1,30 @@
 package edu.grinnell.csc207.soundsofsorting.sortevents;
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  * A <code>CopyEvent</code> logs a copy of a value into an index of the array.
  */
 public class CopyEvent<T> {
-    // TODO: implement me!
+    public int index;
+    public T value;
+
+    public CopyEvent(int idx, T val) {
+        index = idx;
+        value = val;
+    }
+
+    public void apply(T[] arr) {
+        arr[index] = value;
+    }
+
+    public List<Integer> getAffectedIndices(){
+        List<Integer> affectedIndices = new ArrayList<>();
+        affectedIndices.add(index);
+        return affectedIndices;
+    }
+
+    public boolean isEmphasized() {
+        return true;
+    }
 }
