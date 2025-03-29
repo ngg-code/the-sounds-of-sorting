011b04616ccad90e37d1d7886a70e95ff6b3f578
ngg-code <157342451+ngg-code@users.noreply.github.com>
2025-03-29 01:15:14 +0000

    Implemented CompareEvent class

--- src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/CompareEvent.java
+++ src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/CompareEvent.java
@@ -1,4 +1,6 @@
 package edu.grinnell.csc207.soundsofsorting.sortevents;
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  * A <code>CompareEvent</code> logs a comparison a sort makes between two
@@ -6,4 +8,25 @@ package edu.grinnell.csc207.soundsofsorting.sortevents;
  */
 public class CompareEvent<T> {
     // TODO: implement me!
+    private final int index1;
+    private final int index2;
+
+    public CompareEvent(int index1, int index2) {
+        this.index1 = index1;
+        this.index2 = index2;
+    }
+
+    public void apply (T[] arr){
+    }
+
+    public List<Integer> getAffectedIndices(){
+        List<Integer> affectedIndices = new ArrayList<>();
+        affectedIndices.add(index1);
+        affectedIndices.add(index2);
+        return affectedIndices;
+    }
+
+    public boolean isEmphasized(){
+        return true;
+    }
 }
