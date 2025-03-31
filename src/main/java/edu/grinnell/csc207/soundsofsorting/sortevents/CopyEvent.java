0f8f731a31fd26e146a42cb9e57b1298ca0be264
ngg-code <157342451+ngg-code@users.noreply.github.com>
2025-03-31 02:51:12 +0000

    fixed errors

--- src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/CopyEvent.java
+++ src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/CopyEvent.java
@@ -5,25 +5,25 @@ import java.util.ArrayList;
 /**
  * A <code>CopyEvent</code> logs a copy of a value into an index of the array.
  */
-public class CopyEvent<T> {
+public class CopyEvent<T> implements SortEvent<T>{
+    private final int index1;
+    private final int index2;
     public T[] arr;
-    public int index;
-    public T value;
 
-    public CopyEvent(T[] arr, int idx, T value) {
-        index = idx;
-        this.arr = arr; 
-        this.value = value;
+    public CopyEvent(int idx1, int idx2) {
+        this.index1 = idx1;
+        this.index2 = idx2;
     }
 
 
     public void apply(T[] arr) {
-        arr[index] =  value;
+        arr[index1] =  arr[index2];
     }
 
     public List<Integer> getAffectedIndices(){
         List<Integer> affectedIndices = new ArrayList<>();
-        affectedIndices.add(index);
+        affectedIndices.add(index1);
+        affectedIndices.add(index2);
         return affectedIndices;
     }
 
