e21b90f52ddad209086ff2763face3b2e3f2e783
ngg-code <157342451+ngg-code@users.noreply.github.com>
2025-05-02 05:01:07 +0000

    errors

--- src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/CopyEvent.java
+++ src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/CopyEvent.java
@@ -4,49 +4,45 @@ import java.util.List;
 import java.util.ArrayList;
 
 /**
- * A CopyEvent logs a copy of a value into an index of the array.
+ * A <code>CopyEvent</code> logs a copy of a value into an index of the array.
  */
 public class CopyEvent<T> implements SortEvent<T> {
+    private final T value;
     private final int index1;
-    private final int index2;
 
+  
     /**
-     * Constructs a CopyEvent with the given indices
+     * Constructs a <code>CopyEvent</code> with the given value and index.
      * 
-     * @param idx1 the index to copy into
-     * @param idx2 the index to copy from
+     * @param value  the value to copy
+     * @param index1 the index to copy the value into
      */
-    public CopyEvent(int idx1, int idx2) {
-        this.index1 = idx1;
-        this.index2 = idx2;
+    public CopyEvent(T value, int index1) {
+        this.value = value;
+        this.index1 = index1;
     }
-
     /**
-     * Applies the copy event to the given array
+     * Applies the copy event to the given array.
      * 
-     * @param arr the array to copy into
+     * @param arr the array to modify
      */
     public void apply(T[] arr) {
-        arr[index1] = arr[index2];
+        arr[index1] = this.value;
     }
-
     /**
-     * Get affected indices of the copy event
+     * Gets the affected indices of the copy event.
      * 
      * @return list of the affected indices
      */
     public List<Integer> getAffectedIndices() {
         List<Integer> affectedIndices = new ArrayList<>();
         affectedIndices.add(index1);
-        affectedIndices.add(index2);
         return affectedIndices;
     }
-
     /**
-     * Return true if this event should be emphasized by the visualizer/audibilizer.
+     * Returns true if this event should be emphasized by the visualizer/audibilizer.
      * 
-     * @return true if this event should be emphasized by the
-     *         visualizer/audibilizer.
+     * @return true if this event should be emphasized by the visualizer/audibilizer.
      */
     public boolean isEmphasized() {
         return true;
